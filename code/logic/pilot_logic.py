a68e6ba7593c3e8777c37bdfb290a40d7a58a663
Patrik <patrik19@ru.is>
2019-12-04 15:04:30 +0000

    Finished pilot_logic

--- code/logic/pilot_logic.py
+++ code/logic/pilot_logic.py
@@ -7,9 +7,15 @@ class PilotLogic:
     def get_all_pilots():
         return DataAPI.get_all_pilots()
 
+
     @staticmethod
-    def get_licensed_pilots():
-        return []
+    def get_licensed_pilots(pilot_license):
+        all_pilots = DataAPI.get_all_pilots()
+        licensed_pilots = []
+        for pilot in all_pilots:
+            if pilot_license in pilot.get_license():
+                licensed_pilots.append(pilot)
+        return licensed_pilots
 
     @staticmethod
     def save_new_pilot(pilot):
