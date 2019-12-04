588405743ecb7e93e21a2088ab83127253df50f7
Fannar <Fannar90@simnet.is>
2019-12-04 14:43:11 +0000

    klaradi ad gera klasa fyrir state

--- code/data_models/state.py
+++ code/data_models/state.py
@@ -1,26 +1,30 @@
-class State():
+class State:
+    #State for employee and airplane
     __NOT_SCHEDULED_FOR_FLIGHT = "Not scheduled for flight today"
     __LANDED_ABROAD = "Landed abroad"
     __IN_FLIGHT_TO_ICELAND = "In flight to Iceland"
     __IN_FLIGHT_FROM_ICELAND = "In flight from Iceland"
     __WAITING_FOR_FLIGHT_FROM_ICELAND = "Waiting for flight from Iceland"
-   
-   @staticmethod
-    def not_scheduled_for_flight():
-        return State.__NOT_SCHEDULED_FOR_FLIGHT
+    __employee_and_airplane_states = (__NOT_SCHEDULED_FOR_FLIGHT,__WAITING_FOR_FLIGHT_FROM_ICELAND,__IN_FLIGHT_FROM_ICELAND,__LANDED_ABROAD,__IN_FLIGHT_TO_ICELAND)
+    
+    #State for voyages
+    #__WAITING_FOR_FLIGHT_FROM_ICELAND = "Waiting for flight from Iceland"
+    #__IN_FLIGHT_FROM_ICELAND = "In flight from Iceland"
+    __WAITING_FOR_FLIGHT_TO_ICELAND = "Waiting for flight to Iceland"
+    #__IN_FLIGHT_TO_ICELAND = "In flight to Iceland"
+    __VOYAGE_COMPLETED = "Voyage completed"
+    __voyage_state = (__WAITING_FOR_FLIGHT_FROM_ICELAND,__IN_FLIGHT_FROM_ICELAND,__WAITING_FOR_FLIGHT_TO_ICELAND,__IN_FLIGHT_TO_ICELAND,__VOYAGE_COMPLETED)
+
+    @staticmethod
+    def get_employee_state():
+        return State.__employee_and_airplane_states
+
     
     @staticmethod
-    def landed_abroad():
-        return State.__LANDED_ABROAD
+    def get_airplane_state():
+        return State.__employee_and_airplane_states
+
     
     @staticmethod
-    def in_flight_to_iceland():
-        return State.__IN_FLIGHT_TO_ICELAND
-    
-    @staticmethod
-    def in_flight_from_iceland():
-        return State.__IN_FLIGHT_FROM_ICELAND
-    
-    @staticmethod
-    def waiting_for_flight_from_iceland():
-        return State.__WAITING_FOR_FLIGHT_FROM_ICELAND
+    def get_voyage_state():
+        return State.__voyage_state 
