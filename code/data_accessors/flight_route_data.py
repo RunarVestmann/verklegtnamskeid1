0a08db40fab3b8d84433a7ef036b22c9c6f74680
RunnerV <runarvestmann@gmail.com>
2019-12-04 10:22:40 +0000

    Added some functionality

--- code/data_accessors/flight_route_data.py
+++ code/data_accessors/flight_route_data.py
@@ -1,12 +1,23 @@
 from data_models.flight_route import FlightRoute
+import csv
 
 class FlightRouteData:
 
-    __flight_route_data_filename = "flight_routes.csv"
+    __flight_route_data_filename = "../data_storage/flight_routes.csv"
+    __all_flight_routes_list = []
 
     @staticmethod
-    def get_all_flight_attendants():
-        return []
+    def get_all_flight_routes():
+        if not FlightRouteData.__all_flight_routes_list:
+            all_flight_routes_list = []
+            with open(FlightRouteData.__flight_route_data_filename, 'r') as file_stream:
+                reader = csv.DictReader(file_stream)
+                for row in reader:
+                    all_flight_routes_list.append(FlightRoute(row["country"], row["destination"],\
+                        row["airport_id"], row["flight_time"], row["distance_from_iceland"], row["contact_name"], row["emergency_phone"]))
+            FlightRouteData.__all_flight_routes_list = all_flight_routes_list
+
+        return FlightRouteData.__all_flight_routes_list
 
     @staticmethod
     def save_new_flight_attendant(flight_attendant):
