a94a592aebe3419b97170d83bd50e929582eefa9
Patrik <patrik19@ru.is>
2019-12-06 13:58:40 +0000

    Added airport_id function

--- code/logic/flight_route_logic.py
+++ code/logic/flight_route_logic.py
@@ -10,4 +10,12 @@ class FlightRouteLogic:
     @staticmethod
     def get_all_flight_routes():
         return DataAPI.get_all_flight_routes()
+    
+    @staticmethod
+    def is_airport_id_available(airport_id):
+        all_flight_routes = DataAPI.get_all_flight_routes()
+        for FlightRoute in all_flight_routes:
+            if FlightRoute.get_airport_id() == airport_id:
+                return False
+        return True
         
