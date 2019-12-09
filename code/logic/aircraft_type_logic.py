f7718c374980cbba51a478c234ee8772ee242506
RunnerV <runarvestmann@gmail.com>
2019-12-09 20:01:37 +0000

    Sorted by type function added

--- code/logic/aircraft_type_logic.py
+++ code/logic/aircraft_type_logic.py
@@ -4,8 +4,13 @@ from apis.data_api import DataAPI
 class AircraftTypeLogic:
 
     @staticmethod
+    def __sort_list_by_plane_type(a_list):
+        '''Returns a new list sorted in alphabetical plane type order, returns [] if the list was empty'''
+        return a_list if not a_list else sorted(a_list, key=lambda value: value.get_plane_type())
+
+    @staticmethod
     def get_all_aircraft_types():
-        return DataAPI.get_all_aircraft_types()
+        return AircraftTypeLogic.__sort_list_by_plane_type(DataAPI.get_all_aircraft_types())
 
     @staticmethod
     def get_aircraft_type(plane_type):
