bfd84385187ea217de3012771e161ce35187c422
Patrik <patrik19@ru.is>
2019-12-10 14:18:50 +0000

    Added profile selection screen in show_airplane function

--- code/user_interface/airplanes_ui.py
+++ code/user_interface/airplanes_ui.py
@@ -8,7 +8,8 @@ from apis.logic_api import LogicAPI
 class AirplanesUI:
     __FRAME_IN_USE_STR = ComponentUI.get_main_options()[2]
     
-
+    navigation_bar_options = ComponentUI.get_navigation_options_tuple()
+    option_tuple = ('Name', 'State', 'Type', 'Manufacturer', "Seats")
     @staticmethod
     def show():
 
@@ -130,17 +131,27 @@ class AirplanesUI:
     def __show_all_airplanes():
         ComponentUI.print_header(AirplanesUI.__FRAME_IN_USE_STR)
         print(TextEditor.format_text("All airplanes", TextEditor.UNDERLINE_TEXT))
- 
-        table_header_tuple = ("Name", "State", "Type", "Manufacturer", "Seats")
-        airplanes_list = LogicAPI.get_all_airplanes()
-        airplanes_getfunctions_tuple = ([airplane.get_name() for airplane in airplanes_list],[airplane.get_state() for airplane in airplanes_list],\
-           [airplane.get_type() for airplane in airplanes_list], [airplane.get_manufacturer() for airplane in airplanes_list], [airplane.get_seat_count() for airplane in airplanes_list])
+    
+        user_input = ""
 
-        ComponentUI.fill_in_table(table_header_tuple, airplanes_getfunctions_tuple, False)
-                 
-        ComponentUI.fill_window_and_print_action_line(len(airplanes_list)+2)
+        while user_input not in AirplanesUI.navigation_bar_options:
+            table_header_tuple = ("Name", "State", "Type", "Manufacturer", "Seats")
+            airplanes_list = LogicAPI.get_all_airplanes()
+            airplanes_getfunctions_tuple = ([airplane.get_name() for airplane in airplanes_list],[airplane.get_state() for airplane in airplanes_list],\
+                [airplane.get_type() for airplane in airplanes_list], [airplane.get_manufacturer() for airplane in airplanes_list], [airplane.get_seat_count() for airplane in airplanes_list])
 
-        return ComponentUI.get_user_input()
+            table_height = len(airplanes_list)
+            ComponentUI.print_frame_table_menu(table_header_tuple, airplanes_getfunctions_tuple, table_height, ComponentUI.print_header(AirplanesUI.__FRAME_IN_USE_STR),"All airplanes")
+            user_input = ComponentUI.get_user_input()
+            user_input = ComponentUI.remove_brackets(user_input)
+            if not user_input.isdigit() or int(user_input) > table_height:
+                    continue
+            table_index = int(user_input)-1
+            airplane = airplanes_list[table_index]        
+
+            user_input = AirplanesUI.__show_airplane(airplane)
+
+        return user_input
 
 
     @staticmethod
@@ -180,3 +191,67 @@ class AirplanesUI:
         ComponentUI.fill_window_and_print_action_line(len(aircrafttype_list)+2)
 
         return ComponentUI.get_user_input()
+
+    @staticmethod
+    def __show_airplane(airplane):
+        user_input = ''
+        user_input_list = [
+            airplane.get_name(),
+            airplane.get_type(),
+            airplane.get_manufacturer(),
+            airplane.get_seat_count(),
+            airplane.get_state()
+        ]
+        valid_user_inputs = ["1"]
+        #ComponentUI.make_valid_menu_options_tuple(len(AirplaneUI.Airplane_OPTION_TUBLE))            
+       
+        airplane_info_already_exists = False
+        while user_input not in AirplanesUI.navigation_bar_options:
+            ComponentUI.print_frame_constructor_menu(AirplanesUI.option_tuple,\
+            ComponentUI.get_main_options()[2], "Airplane name: " + user_input_list[0], user_input_list, True, 1000, [1,2,3,4])
+
+            user_input = ComponentUI.get_user_input()
+            user_input = ComponentUI.remove_brackets(user_input)
+            if user_input in valid_user_inputs: 
+                index = int(user_input) - 1
+                ComponentUI.print_frame_constructor_menu(AirplanesUI.option_tuple,\
+                    ComponentUI.get_main_options()[2], "Airplane name: " + user_input_list[0], user_input_list, False, 1000, [1,2,3,4])
+
+                if(index == 0):
+                    user_input = input("Insert name: ").strip()
+                
+
+
+
+                #Capitalize the first letters in the contacts name
+                if index == 0:
+                    if not user_input: #use existing name in case if user cancels or leaves it blank 
+                        user_input = user_input_list[index]
+
+                    airplane_name_list = []
+                    for name in user_input.split():
+                        airplane_name_list.append(name.capitalize())
+                    user_input = " ".join(airplane_name_list)
+
+              
+                
+                user_input_list[index] = user_input
+                user_input = ""
+                   
+            elif user_input.startswith('s'):
+                if all(user_input_list) and not airplane_info_already_exists:
+
+                    edited_airplane = Airplane(
+                        user_input_list[0],
+                        user_input_list[1],
+                        user_input_list[2],
+                        user_input_list[3],
+                        user_input_list[4]
+
+                    )
+
+                    LogicAPI.change_saved_airplane(airplane, edited_airplane)
+                    #þarfnast skoðunnar(Kannski er þetta ok þar sem breytingar sjást, næ ekki að láta þetta virka heldur)
+                    user_input = "An airplane has been edited"
+                    break
+        return user_input
