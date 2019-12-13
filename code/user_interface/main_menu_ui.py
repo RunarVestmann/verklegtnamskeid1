3c1adcd6c2521d953b151fd601c2f7c44083afc7
Guðmundur Sverrisson <gudmundursvr@gmail.com>
2019-12-13 20:48:42 +0000

    comments and fix

--- code/user_interface/main_menu_ui.py
+++ code/user_interface/main_menu_ui.py
@@ -5,32 +5,14 @@ class MainMenuUI:
 
     @staticmethod
     def __print_main_menu_body():
+        ''' message on first screen '''
         ComponentUI.centered_text_message("Welcome to the NaN Air booking software",\
             "Press any of the keys in the brackets '()' to get started")
             
-        # window_width, window_height = Window.get_size()
-
-        # #Calculate the how much space is left on the window
-        # body_height = window_height - ComponentUI.get_header_height()
-
-        # #Calculate how many new lines should be both above and below the quit text
-        # offsetted_body_height_center = (body_height//2) - 2
-
-        # #Print the empty space above the welcome text
-        # for _ in range(offsetted_body_height_center):
-        #     print()
-
-        # print("Welcome to the NaN Air booking software".center(window_width))
-        # print("Press any of the keys in the brackets '()' to get started".center(window_width))
-
-        # #Print the empty space below the welcome text
-        # for _ in range(offsetted_body_height_center):
-        #     print()
-
-        # print("_" * window_width)
 
     @staticmethod
     def show():
+        ''' Displaying message on the center of when program is opened '''
         ComponentUI.print_header()
         MainMenuUI.__print_main_menu_body()
 
